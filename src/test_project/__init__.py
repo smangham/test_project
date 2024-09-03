5d37109bfd80034501e60298ec0a7c5384433637
Sam Mangham <mangham@gmail.com>
2024-09-03 14:05:44 +0100

    Fixed missing src file

--- src/test_project/__init__.py
+++ src/test_project/__init__.py
@@ -5,5 +5,5 @@ import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Sam Mangham"
-__email__ = "mangham@gmail.om"
+__email__ = "mangham@gmail.com"
 __version__ = "0.1.0"
